2c7d12b6ab098762a748df4519931a95430472b5
Vincent Heeren <vincent.heeren@student.ucll.be>
2018-10-17 17:36:19 +0200

    story 1 WIP + gitignore

---
+++ ProjectMobileApp/ProjectMobileApp/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectMobileApp.Model
+{
+    public enum Category
+    {
+        Food,
+        Transport,
+        Clothing,
+        Education,
+        Leisure,
+        Other
+    }
+}
---
+++ ProjectMobileApp/ProjectMobileApp/Model/DomainException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectMobileApp.Model
+{
+    class DomainException : Exception
+    {
+        public DomainException() : base()
+        {
+        }
+
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
