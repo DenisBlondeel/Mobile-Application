b9ab23b117e8cd7d107034defceeca60c3163a1a
Denis Blondeel <denisblondeel@hotmail.com>
2018-12-05 17:19:52 +0100

    Added NuGet & JSON.Net & serialize / deserialize methods in repo

--- ProjectMobileApp/ProjectMobileApp/Database/PaymentDbRemote.cs
+++ ProjectMobileApp/ProjectMobileApp/Database/PaymentDbRemote.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using ProjectMobileApp.Model;
 
 namespace ProjectMobileApp.Database
 {
-    class PaymentDbRemote
+    class PaymentDbRemote : PaymentDb
     {
+        public void AddPayment(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePayment(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Payment GetPayment(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Payment> GetPayments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePayment(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        private String serialize(Payment payment)
+        {
+            return JsonConvert.SerializeObject(payment, Formatting.Indented);
+        }
+
+        private Payment deserialise(String json)
+        {
+           return JsonConvert.DeserializeObject<Payment>(json);
+        }
+
     }
 }
