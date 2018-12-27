ba12731efbfcada8da21abb3d566b59e48bf3ae3
Denis Blondeel <denisblondeel@hotmail.com>
2018-12-27 17:00:40 +0100

    Register tool

--- ProjectMobileApp/ProjectMobileApp/App.xaml.cs
+++ ProjectMobileApp/ProjectMobileApp/App.xaml.cs
@@ -12,7 +12,7 @@ namespace ProjectMobileApp
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new RegisterPage());
         }
 
         protected override void OnStart()
---
+++ ProjectMobileApp/ProjectMobileApp/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ProjectMobileApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ProjectMobileApp.ViewModel
+{
+    class RegisterViewModel
+    {
+
+        ApiServices _apiServices = new ApiServices();
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string Message { get; set; }
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    var isSuccess = await _apiServices.RegisterAsync(Email, Password, ConfirmPassword);
+
+                    if (isSuccess)
+                        Message = "Successfully registered";
+                    else
+                    {
+                        Message = "Failed";
+                    }
+                });
+            }
+        }
+    }
+}
